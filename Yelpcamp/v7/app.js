66830ceb3bcbc6cf75f26cc4e343e36cf16c2cb6
WillLai <252926752@qq.com>
2019-07-03 20:49:11 +0800

    completed v7

---
+++ Yelpcamp/v7/app.js
@@ -0,0 +1,62 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
+    Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
+    User        = require("./models/user"),
+    seedDB      = require("./seeds");
+
+var commentRoutes    = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes      = require("./routes/index");
+
+seedDB();
+mongoose.set('useNewUrlParser', true);
+mongoose.connect("mongodb://localhost/yelpcamp");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.use(require("express-session")({
+    secret:"once again yangyao is my son",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+app.use(indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
+
+
+
+app.listen(2000, function(){
+   console.log("The YelpCamp Server Has Started!");
+});
